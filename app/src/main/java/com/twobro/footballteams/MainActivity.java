1f298d4bb13087270194528da8608e0c15ee5cbf
AntonSevas <overton8889@gmail.com>
2022-10-18 19:39:21 +0500

    Ver.0.3

--- app/src/main/java/com/twobro/footballteams/MainActivity.java
+++ app/src/main/java/com/twobro/footballteams/MainActivity.java
@@ -1,14 +1,49 @@
 package com.twobro.footballteams;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.twobro.footballteams.adapter.CategoryAdapter;
+import com.twobro.footballteams.model.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView categoryRecycler;
+    CategoryAdapter categoryAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1,"Игры"));
+        categoryList.add(new Category(2,"Клубы"));
+        categoryList.add(new Category(3,"Тренер"));
+        categoryList.add(new Category(4,"Чемп"));
+
+        setCategoryRecycler(categoryList);
+
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList) {
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false);
+
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        categoryAdapter = new CategoryAdapter(this,categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
+
+
+
     }
 }
---
+++ app/src/main/java/com/twobro/footballteams/adapter/CategoryAdapter.java
@@ -0,0 +1,57 @@
+package com.twobro.footballteams.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.twobro.footballteams.R;
+import com.twobro.footballteams.model.Category;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
+
+    Context context;
+    List<Category> categories;
+
+    public CategoryAdapter(Context context, List<Category> categories) {
+        this.context = context;
+        this.categories = categories;
+    }
+
+    @NonNull
+    @Override
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View categoryItems = LayoutInflater.from(context).inflate(R.layout.category_item, parent, false);
+        return new CategoryViewHolder(categoryItems);
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
+        holder.categoryTitle.setText(categories.get(position).getTitle());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.size();
+    }
+
+    public static final class  CategoryViewHolder extends RecyclerView.ViewHolder {
+
+        TextView categoryTitle;
+
+        public CategoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            categoryTitle = itemView.findViewById(R.id.categoryTitle);
+        }
+    }
+}
