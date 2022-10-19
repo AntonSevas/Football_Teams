95e0edc9fe089dd45452b65eafff418c8fa81850
AntonSevas <overton8889@gmail.com>
2022-10-19 19:58:21 +0500

    Ver.0.5

--- app/src/main/java/com/twobro/footballteams/MainActivity.java
+++ app/src/main/java/com/twobro/footballteams/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     CategoryAdapter categoryAdapter;
 
 
-    @Override
+    @Override0
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
---
+++ app/src/main/java/com/twobro/footballteams/Override0.java
@@ -0,0 +1,4 @@
+package com.twobro.footballteams;
+
+public @interface Override0 {
+}
